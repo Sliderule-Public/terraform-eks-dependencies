b8a89b42f39e842de69f4cb1b24361b5c73f32da
Matt Martinez <matthewmartinez1003@gmail.com>
2022-09-23 06:54:03 -0500

    remove variables

--- main.tf
+++ main.tf
@@ -10,7 +10,6 @@ module "main" {
   kms_grantees                             = var.kms_grantees
   snapshot_identifier                      = var.snapshot_identifier
   alarms_email_recipients                  = var.alarms_email_recipients
-  server_iam_role_policy_statements        = var.server_iam_role_policy_statements
   skip_final_snapshot                      = var.skip_final_snapshot
   deploy_read_replica                      = var.deploy_read_replica
   database_security_group_additional_rules = var.database_security_group_additional_rules
@@ -23,11 +22,8 @@ module "main" {
   web_eks_port                             = var.web_eks_port
   docs_eks_port                            = var.docs_eks_port
   api_eks_port                             = var.api_eks_port
-  use_variable_scripts                     = var.use_variable_scripts
-  certificate_arn                          = var.certificate_arn
   database_instance_type                   = var.database_instance_type
   eks_cluster_name                         = var.eks_cluster_name
-  app_name                                 = var.app_name
   iam_arns_to_grant_sns_kms_access_to      = var.iam_arns_to_grant_sns_kms_access_to
   use_only_private_subnets                 = var.use_only_private_subnets
   tags = {
