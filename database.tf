ab64c2d403ab3e0a9cded724c3ea6e9b03d825cd
Matt Martinez <matthewmartinez1003@gmail.com>
2023-11-09 14:23:06 -0600

    upgrade postgres 14 version

--- database.tf
+++ database.tf
@@ -20,6 +20,6 @@ module "rds_instance" {
   instance_type            = var.database_instance_type
   deploy_read_replica      = var.deploy_read_replica
   use_only_private_subnets = var.use_only_private_subnets
-  rds_engine_version       = "14.6"
+  rds_engine_version       = "14.7"
 
 }
