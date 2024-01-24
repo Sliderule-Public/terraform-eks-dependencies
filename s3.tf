b30d0d078b5023b674811efe1be875db83148dad
Matt Martinez <matthewmartinez1003@gmail.com>
2024-01-24 09:37:11 -0600

    use new s3 module version

--- s3.tf
+++ s3.tf
@@ -1,18 +1,28 @@
 module "infrastructure_bucket" {
-  source       = "github.com/Modern-Logic/terraform-modules.git//simple/s3_bucket?ref=v1.0"
-  environment  = var.environment
-  region       = var.region
-  company_name = var.company_name
-  account_id   = local.account_id
-  bucket_name  = "infrastructure-eks"
-  key_arn      = module.main_key.key_arn
-  tags         = var.tags
+  providers = {
+    aws                          = aws
+    aws.cross_region_replication = aws.cross_region_replication
+  }
+  source             = "github.com/Modern-Logic/terraform-modules.git//simple/s3_bucket?ref=v1.8"
+  environment        = var.environment
+  region             = var.region
+  replication_region = var.cross_region_replication_region
+  company_name       = var.company_name
+  account_id         = local.account_id
+  bucket_name        = "infrastructure-eks"
+  key_arn            = module.main_key.key_arn
+  tags               = var.tags
 }
 
 module "server_docs_bucket" {
-  source                    = "github.com/Modern-Logic/terraform-modules.git//simple/s3_bucket?ref=v1.0"
+  providers = {
+    aws                          = aws
+    aws.cross_region_replication = aws.cross_region_replication
+  }
+  source                    = "github.com/Modern-Logic/terraform-modules.git//simple/s3_bucket?ref=v1.8"
   environment               = var.environment
   region                    = var.region
+  replication_region        = var.cross_region_replication_region
   company_name              = var.company_name
   account_id                = local.account_id
   bucket_name               = "server-documents"
