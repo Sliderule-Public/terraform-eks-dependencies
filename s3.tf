bed7dc54b7e8fc3d69c9589c1183b0c2018f9278
Matt Martinez <matthewmartinez1003@gmail.com>
2024-02-08 14:40:30 -0600

    add optional s3 replication

--- s3.tf
+++ s3.tf
@@ -3,15 +3,16 @@ module "infrastructure_bucket" {
     aws                          = aws
     aws.cross_region_replication = aws.cross_region_replication
   }
-  source             = "github.com/Modern-Logic/terraform-modules.git//simple/s3_bucket?ref=v1.10.2"
-  environment        = var.environment
-  region             = var.region
-  replication_region = var.cross_region_replication_region
-  company_name       = var.company_name
-  account_id         = local.account_id
-  bucket_name        = "infrastructure-eks"
-  key_arn            = module.main_key.key_arn
-  tags               = var.tags
+  source                           = "github.com/Modern-Logic/terraform-modules.git//simple/s3_bucket?ref=v1.10.3"
+  environment                      = var.environment
+  region                           = var.region
+  replication_region               = var.cross_region_replication_region
+  company_name                     = var.company_name
+  account_id                       = local.account_id
+  bucket_name                      = "infrastructure-eks"
+  key_arn                          = module.main_key.key_arn
+  tags                             = var.tags
+  deploy_cross_region_read_replica = var.deploy_cross_region_read_replica
 }
 
 module "server_docs_bucket" {
@@ -19,14 +20,15 @@ module "server_docs_bucket" {
     aws                          = aws
     aws.cross_region_replication = aws.cross_region_replication
   }
-  source                    = "github.com/Modern-Logic/terraform-modules.git//simple/s3_bucket?ref=v1.10.2"
-  environment               = var.environment
-  region                    = var.region
-  replication_region        = var.cross_region_replication_region
-  company_name              = var.company_name
-  account_id                = local.account_id
-  bucket_name               = "server-documents"
-  key_arn                   = module.main_key.key_arn
-  upload_cors_rules_enabled = true
-  tags                      = var.tags
+  source                           = "github.com/Modern-Logic/terraform-modules.git//simple/s3_bucket?ref=v1.10.3"
+  environment                      = var.environment
+  region                           = var.region
+  replication_region               = var.cross_region_replication_region
+  company_name                     = var.company_name
+  account_id                       = local.account_id
+  bucket_name                      = "server-documents"
+  key_arn                          = module.main_key.key_arn
+  upload_cors_rules_enabled        = true
+  tags                             = var.tags
+  deploy_cross_region_read_replica = var.deploy_cross_region_read_replica
 }
